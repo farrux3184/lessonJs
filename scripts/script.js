1d90f73d81e0d55d27c1af29b1e02d77df412a98
farrux djanbaev <farrux3184@mail.ru>
2020-05-02 19:21:50 +0300

    2

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,18 @@
 'use strict';
 
-const body = document.querySelector('body');
-
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
+  }
 }
 
-DomElement.prototype.render = function () {
-  let addElement;
-  if (this.selector[0] === '.') {
-    addElement = document.createElement('div');
-    addElement.classList.add('block');
-  } else if (this.selector[0] === '#') {
-    addElement = document.createElement('p');
-    addElement.classList.add('best');
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
   }
+}
 
-  addElement.style.cssText =` 
-    height:  ${this.height}px;
-    width:  ${this.width}px;
-    background:  ${this.bg};
-    font-size:  ${this.fontSize}px;`
-  
-  addElement.textContent = 'hello world';
-  body.append(addElement);
-  console.log('addElement: ', addElement);
-};
+let myClass = new Second();
+myClass.hello();
 
-let newDomElements = new DomElement('.id', 150, 200, 'blue', 36);
-newDomElements.render();
-
-let newDomElements2 = new DomElement('#id', 75, 100, 'red', 20);
-newDomElements2.render();
